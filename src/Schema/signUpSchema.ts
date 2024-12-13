e8765c6cab996c4574a7a1a5588f080824c1db40
Aman Kumar Jha <amankumar9631400@gmail.com>
2024-12-13 23:57:05 +0530

    Sign up  FORM from react hook sand shadcn😎😎

--- src/Schema/signUpSchema.ts
+++ src/Schema/signUpSchema.ts
@@ -2,8 +2,8 @@ import { z } from "zod";
 
 export const usernameValidation = z
   .string()
-  .min(2, "username mush be 2 charactors")
-  .max(20, "username must not contain more than 20 charactors")
+  .min(2, "Username must have atleast 2 charactors")
+  .max(20, "Username must not contain more than 20 charactors")
   .regex(/^[a-zA-z0-9_]+$/, "Username must not contain special charactor");
 
 export const signUpSchema = z.object({
@@ -11,5 +11,5 @@ export const signUpSchema = z.object({
   email: z.string().email({ message: "Invalid email address" }),
   password: z
     .string()
-    .min(6, { message: "password must be atleast 6 charactors" }),
+    .min(6, { message: "Password must be atleast 6 charactors" }),
 });
--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,61 +1,9 @@
-"use client";
-import React, { useEffect, useState } from "react";
-import * as z from "zod";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import Link from "next/Link";
-import { useDebounceValue } from "usehooks-ts";
-import { useToast } from "@/hooks/use-toast";
-import { useRouter } from "next/navigation";
-import { signUpSchema } from "@/Schema/signUpSchema";
-import axios,{AxiosError} from 'axios'
-import { ApiResponse } from "../../../../types/ApiResponse";
+import React from 'react'
 
 const page = () => {
-  const [username, setUsername] = useState("");
-  const [usernameMessage, setUsernameMessage] = useState("");
-  const [isCheckingUsername, setIsCheckingUsername] = useState(false);
-  const [isSubmitting, setIsSubmitting] = useState(false);
+  return (
+    <div>sign in - page</div>
+  )
+}
 
-  // it will reflect changes in username value after 300ms
-  const debouncedUsername = useDebounceValue(username, 300);
-  const { toast } = useToast();
-  const router = useRouter();
-
-  // zod implementation
-  const form = useForm<z.infer<typeof signUpSchema>>({
-    resolver: zodResolver(signUpSchema),
-    defaultValues: {
-      username: "",
-      email: "",
-      password: "",
-    }
-  });
-
-  useEffect(() => {
-    const chechUserUnique = async () => {
-      if (debouncedUsername) {
-        setIsCheckingUsername(true)
-        setUsernameMessage('')
-
-        try {
-          const res = await axios.get(`/api/check-username-unique?username=${debouncedUsername}`);
-          setUsernameMessage(res.data.message);
-        } catch (error) {
-          const axiosError = error as AxiosError<ApiResponse>;
-          setUsernameMessage(axiosError.response?.data.message ?? "Error checking username")
-        }
-        finally {
-          setIsCheckingUsername(false)
-        }
-      }
-    }
-
-    chechUserUnique();
-  } , [debouncedUsername])
-
-
-  return <div>page</div>;
-};
-
-export default page;
+export default page
